131e81f3f861d34749a4903eac1bdbf93a527c8f
sampanchan <spankratz@my.creativecircus.edu>
2021-02-16 18:44:06 -0500

    lol

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,4 @@ console.log(`Hello World from main.js!
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
 
-console.log('spark spark boo BOOM!')
+console.log('spark spark boo BOOM! LOL')
