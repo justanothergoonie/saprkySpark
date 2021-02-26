e0429c94092a41be6affe4b48188079585614fba
Cody J Powell <codyj.powell@outlook.com>
2021-02-26 12:26:28 -0500

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,10 +2,11 @@
 
 console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
 var hamburgerButton = document.querySelector('.hamburger');
-var navList = document.querySelector('nav');
+var navList = document.querySelector('.content');
 
 var toggleNav = function toggleNav() {
   navList.classList.toggle('showing');
+  hamburgerButton.classList.toggle('focus');
 };
 
 hamburgerButton.addEventListener('click', toggleNav);
--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,10 @@ Change this message, and make sure it changes in the browser
 to verify that you're working in the right files.`);
 let hamburgerButton = document.querySelector('.hamburger');
 
-let navList = document.querySelector('nav');
+let navList = document.querySelector('.content');
 
 let toggleNav = function () {
 	navList.classList.toggle('showing');
+	hamburgerButton.classList.toggle('focus');
 };
 hamburgerButton.addEventListener('click', toggleNav);
